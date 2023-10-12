0ed4a3622d458c9fa0c4dac957422ed2c5f91b7a
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-10-12 21:08:49 +0000

    removed tempate render methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,6 @@
 class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
-
-    render "movies/new"
   end
 
   def index
@@ -28,7 +26,6 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render "movies/show" 
   end
 
   def create
@@ -51,7 +48,6 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render "movies/edit"
   end
 
   def update
